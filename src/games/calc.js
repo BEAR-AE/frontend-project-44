d20525ae310c99c7f7a2faf8d6b13fd71d2f7877
BEAR-AE <eremin.alek2014@yandex.ru>
2024-09-09 23:26:41 +0300

    'fix bin/brain-calc.js'

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,31 +1,40 @@
-import startGame from '../index.js';
-import getRandomNumber from '../getRandomNumber.js';
-import getRandomInt from '../getRandomInt.js';
+import startGame from "../index.js";
+import getRandomInt from "../getRandomInt.js";
+import getRandomNumber from "../getRandomNumber.js";
 
-const takeConditions = 'What is the result of the expression?';
+const startCalc = () => {
+  const noteToCalc = "What is the result of the expression?";
 
-const toSigns = ['+', '-', '*'];
+  const taskCalc = () => {
+    const number1 = getRandomNumber();
+    const number2 = getRandomNumber();
 
-const getCalc = (a, b, sign) => {
-  switch (sign) {
-    case '-':
-      return a - b;
-    case '+':
-      return a + b;
-    case '*':
-      return a * b;
-    default:
-      throw new Error('invalid expression');
-  }
+    const signs = ["+", "-", "*"];
+    const sign = signs[getRandomInt(signs.length)];
+
+    const question = `${number1} ${sign} ${number2}`;
+
+    let result = 0;
+    switch (sign) {
+      case "+":
+        result = number1 + number2;
+        break;
+      case "-":
+        result = number1 - number2;
+        break;
+      case "*":
+        result = number1 * number2;
+        break;
+      default:
+        result = null;
+    }
+
+    result = result.toString();
+
+    return [question, result];
+  };
+
+  startGame(noteToCalc, taskCalc);
 };
 
-const getQuestionAndAnswer = () => {
-  const firstValue = getRandomNumber();
-  const secondVavue = getRandomNumber();
-  const sign = toSigns[getRandomInt(0, toSigns.length - 1)];
-  const question = `${firstValue} ${sign} ${secondVavue}`;
-  const correctAnswer = getCalc(firstValue, secondVavue, sign).toString();
-  return [question, correctAnswer];
-};
-
-export default () => startGame(getQuestionAndAnswer, takeConditions);
+export default startCalc;
