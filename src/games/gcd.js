dbfc4a056c0162f61815b2640e1ee75f4cccabef
BEAR-AE <eremin.alek2014@yandex.ru>
2024-09-10 21:56:31 +0300

    'fix src/games/gcd.js'

---
+++ src/games/gcd.js
@@ -0,0 +1,26 @@
+import startGame from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const calculateGcd = (number1, number2) => {
+  if (number2 === 0) {
+    return number1;
+  }
+  return calculateGcd(number2, number1 % number2);
+};
+
+const toCalcConditions = (getGcd) => (number1, number2) => getGcd(number1, number2);
+
+const startGCD = () => {
+  const noteToGcd = 'Find the greatest common divisor of given numbers.';
+  const taskGcd = () => {
+    const number1 = getRandomNumber();
+    const number2 = getRandomNumber();
+    const getGcd = toCalcConditions(calculateGcd);
+    const question = `${number1} ${number2}`;
+    const result = getGcd(number1, number2).toString();
+    return [question, result];
+  };
+  startGame(noteToGcd, taskGcd);
+};
+
+export default startGCD;
