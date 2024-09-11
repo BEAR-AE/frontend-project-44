a4f9fe0a1afc11c1ac49359607b52ab1bbdb0d2d
BEAR-AE <eremin.alek2014@yandex.ru>
2024-09-11 04:19:23 +0300

    'fix src/getRandomNumber.js'

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-irregular-whitespace */
 const getRandomNumber = () => Math.floor(Math.random() * 50);
 
-export default getRandomNumber;
+export default getRandomNumber; // range of values ​​is from 0 to 49
